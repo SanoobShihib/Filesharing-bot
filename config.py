9ab61905028505e51e68aa6c8f8b2af9f7649dab
Sanoob Achu 🌝 <sanoobsanoop130@gmail.com>
2026-09-25 22:32:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,6 +9,7 @@ API_HASH = os.getenv("API_HASH")
 
 # Required Channels
 CHANNELS = os.getenv("CHANNELS", "").split()
+CHANNEL_INVITE_LINKS = os.getenv("CHANNEL_INVITE_LINKS", "").split()
 
 # Database Configuration
 DATABASE_URI = os.getenv("DATABASE_URI")
